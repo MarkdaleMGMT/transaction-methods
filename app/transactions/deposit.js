574b6b78b5c0a72c54e43bb6eb563e1a33eb3b7e
Carlson <carl.lau999@gmail.com>
2019-01-10 20:11:04 -0500

    test

--- app/transactions/deposit.js
+++ app/transactions/deposit.js
@@ -54,6 +54,7 @@ const {build_update_user_balance, get_user_by_username} = require('../models').u
 
      console.log("rows affected",rows_affected);
      let previous_balance = await get_user_by_username(username)
+     console.log("previous balance", previous_balance)
      let update_query =  build_update_user_balance(previous_balance.clam_balance + amount)
      let current_balance = await db.connection.query(update_query.query, update_query.queryValues)
 
