358cd8391961748bdbceb7e4cec43e0c88a030d3
Ayesha Hafeez <ayesha.h@live.com>
2019-02-13 14:08:32 -0500

    Updated prestart script
    Signed-off-by: Ayesha Hafeez <ayesha.h@live.com>

--- scripts/npm/prestart.sh
+++ scripts/npm/prestart.sh
@@ -5,14 +5,14 @@ read -r -p "Have you configured .env file? (Y/N) " input
 
 case $input in
     [yY][eE][sS]|[yY])
- echo "Environment variables configuration ... done"
+ printf "Environment variables configuration ... done"
  ;;
     [nN][oO]|[nN])
- echo "Deployment will not continue until environment variables are set"
+ printf "ERROR!!!:\nDeployment will not continue until environment variables are set. \nRename 'sample.env' to '.env' and update the configurations"
  exit 1
        ;;
     *)
- echo "Invalid input..."
+ printf "Invalid input..."
  exit 1
  ;;
 esac #end case
