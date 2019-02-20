0ea536dc9d5917367e0ac10721049b8d5cf82c3a
Ayesha Hafeez <ayesha.h@live.com>
2019-02-20 09:59:32 -0500

    Changed loop for scheduling cronjob

--- scripts/cronjobs/schedule_cronjobs.sh
+++ scripts/cronjobs/schedule_cronjobs.sh
@@ -17,17 +17,19 @@ cur=$(crontab -l)
 job1="0 0 * * * $dir/db_backup.sh $mysql_username $mysql_password $db_name" #backup the live db
 job2="0 0 * * * $dir/scrape_address_balances.sh"
 
-for value in {$job1,$job2}
+jobs=(job1,job2)
+
+for job in "${jobs[@]}"
 do
-  if [[ $(crontab -l | egrep -v '^(#|$)' | grep -q "$escapedEntry"; echo $value) == 1 ]] # from: https://unix.stackexchange.com/a/297377/320236
+  if [[ $(crontab -l | egrep -v '^(#|$)' | grep -q "$escapedEntry"; echo $job) == 1 ]] # from: https://unix.stackexchange.com/a/297377/320236
   then
-      printf "all clear; pattern was not already present; adding command to crontab hourly:\n$value\n\n"
-      (crontab -l ; printf "$value\n\n") | crontab -
+      printf "all clear; pattern was not already present; adding command to crontab hourly:\n$job\n\n"
+      (crontab -l ; printf "$job\n\n") | crontab -
   else
       printf "pattern already present; no action taken\n\n"
   fi
 
-  echo "$cur$value" | crontab -
+  echo "$cur$job" | crontab -
 done
 
 
