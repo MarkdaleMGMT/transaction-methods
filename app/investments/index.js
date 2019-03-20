6fee5a9cf3e7b7a90adccc0c60eadcae2c992d5b
Ayesha Hafeez <ayesha.h@live.com>
2019-03-20 04:01:35 -0400

    changed GET to POST

--- app/investments/index.js
+++ app/investments/index.js
@@ -3,4 +3,4 @@ const app = module.exports = require('express')();
 
 //define the endpoints
 // app.post('/balance_sheet', require('./balance_sheet'));
-app.get('/accounts', require('./accounts'));
+app.post('/accounts', require('./accounts'));
