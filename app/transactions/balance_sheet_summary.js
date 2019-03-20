71e4681c2c628b3f357063b8f5f85d768f192328
Ayesha Hafeez <ayesha.h@live.com>
2019-03-20 04:27:17 -0400

    Modified API to retreive balance sheet per investment

--- app/transactions/balance_sheet_summary.js
+++ app/transactions/balance_sheet_summary.js
@@ -1,20 +1,23 @@
 var db = require('../util/mysql_connection')
 const { get_transactions_summary } = require('../models').transaction_model
 const { get_user_by_username } = require('../models').user_model
-const { get_all_investments } = require('../models').investment_model
+const { get_all_investments, get_investment_by_id } = require('../models').investment_model
 const { get_account_by_id } = require('../models').account_model
 
 
 
 
 /**
- * API to get the summary of the balance sheet for all investments
+ * API to get the summary of the balance sheet for all investments or a specific investment if investment_id is specified
+ * @param  {int} investment_id (optional)    Amount to be deposited
+ * @return {JSON}         Returns success
  */
  // TODO: add investment type
  module.exports = async function balance_sheet_summary_api(req, res) {
 
    try{
-     let summary = await balance_sheet_summary();
+     let investment_id = req.body.investment_id;
+     let summary = await balance_sheet_summary(investment_id);
      res.send(summary);
    }
    catch(err){
@@ -24,17 +27,31 @@ const { get_account_by_id } = require('../models').account_model
  };
 
 
- async function balance_sheet_summary(){
+ async function balance_sheet_summary(investment_id){
 
 
    try{
 
+     console.log("got investment_id ", investment_id);
+
      let investment_tx_summary = [];
-     let investments = await get_all_investments();
+     var investments = null;
+     if(investment_id == null || investment_id == undefined){
+       investments = await get_all_investments();
+     }
+     else{
+
+       let single_investment = await get_investment_by_id(investment_id);
+       investments = [ single_investment ]
+       console.log("id",investments);
+     }
+
      console.log("no . of investments",investments.length);
 
      //TODO: iterate over each investment
      for(let i=0; i<investments.length; i++){
+
+       console.log("i ",i);
 
        let debit_entries = [];
        let credit_entries = [];
