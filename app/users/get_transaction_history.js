90df8afcc1d457d331e8c66a3657036221de68a8
Ayesha Hafeez <ayesha.h@live.com>
2019-03-20 16:29:07 -0400

    updated date format

--- app/users/get_transaction_history.js
+++ app/users/get_transaction_history.js
@@ -57,7 +57,7 @@ const { get_user_by_username } = require('../models').user_model
 
 
       let transaction_json = {
-        'time':dateFormat(new Date(user_transaction.time),'dd mmm yyyy, h:MM:ss TT'),
+        'time':dateFormat(new Date(user_transaction.time),'yyyy mm dd , HH:MM:ss'),
         'description': user_transaction.memo,
         'amount':Math.abs(user_transaction.amount),
         'type': user_transaction.amount <0 ? 'credit':'debit',
