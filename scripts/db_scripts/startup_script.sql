48fa27c945851a7bb15868cf99f4a4c7f16b0bee
jack <nian.jack@gmail.com>
2019-05-11 19:35:59 +0000

    added startup script

---
+++ scripts/db_scripts/startup_script.sql
@@ -0,0 +1,6 @@
+INSERT INTO user
+(username, password, level, email, email_verify_key, email_verify_flag, affiliate)
+VALUES
+('admin', '$2b$10$IuWpARdQuMBJHu2cnDIfdede0bZy428ViuvbK5MME4v4OOAWocNCW', 0, NULL, '', 1, NULL),
+('investment_user', '$2b$10$IuWpARdQuMBJHu2cnDIfdede0bZy428ViuvbK5MME4v4OOAWocNCW', 0, NULL, '', 1, NULL),
+('rake_user', '$2b$10$IuWpARdQuMBJHu2cnDIfdede0bZy428ViuvbK5MME4v4OOAWocNCW', 0, NULL, '', 1, NULL)
