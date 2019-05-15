444a5e743c0b0fad00cbdcde5a2b64edae808a07
Ayesha Hafeez <ayesha.h@live.com>
2019-05-15 13:00:04 -0400

    change crontask syntax
    Signed-off-by: Ayesha Hafeez <ayesha.h@live.com>

--- scripts/cronjobs/populate_exchange_rates.sh
+++ scripts/cronjobs/populate_exchange_rates.sh
@@ -2,16 +2,16 @@
 
 
 printf "logging new rates from scotiabank...\n"
-curl -o -d '{"source":"scotiabank", "rates_for":["USD_CAD"]}' -H "Content-Type: application/json" -X POST http://localhost:3000/fx/update_exchange_rates
+curl -d '{"source":"scotiabank", "rates_for":["USD_CAD"]}' -H "Content-Type: application/json" -X POST http://localhost:3000/fx/update_exchange_rates
 
 printf "\ndone...\n"
 
 printf "\nlogging new rates from poloniex...\n"
-curl -o -d '{"source":"poloniex", "rates_for":["CLAM_BTC"]}' -H "Content-Type: application/json" -X POST http://localhost:3000/fx/update_exchange_rates
+curl -d '{"source":"poloniex", "rates_for":["CLAM_BTC"]}' -H "Content-Type: application/json" -X POST http://localhost:3000/fx/update_exchange_rates
 
 printf "\ndone...\n"
 
 printf "logging new rates from bitfinex...\n"
-curl -o -d '{"source":"bitfinex", "rates_for":["BTC_USD"]}' -H "Content-Type: application/json" -X POST http://localhost:3000/fx/update_exchange_rates
+curl -d '{"source":"bitfinex", "rates_for":["BTC_USD"]}' -H "Content-Type: application/json" -X POST http://localhost:3000/fx/update_exchange_rates
 
 printf "done...\n"
