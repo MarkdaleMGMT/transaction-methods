6acbd54e16687c0cac00a1fb07a7c58d665534b9
jack <nian.jack@gmail.com>
2019-05-24 12:15:19 +0000

    removed deploy json

--- sample-ecosystem.config.js
+++ sample-ecosystem.config.js
@@ -29,17 +29,6 @@ module.exports = {
       INVESTMENT_ACNT:<investment_acnt_username>,
       RAKE_ACNT:<rake_acnt_username>
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
 
