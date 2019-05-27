763fb06c3f3d7ad93967b8453d38882c71ecbb07
Ayesha Hafeez <ayesha.h@live.com>
2019-05-27 16:59:21 +0000

    added log statements

--- scripts/clam_miner/bootstrap.js
+++ scripts/clam_miner/bootstrap.js
@@ -11,10 +11,13 @@ async function sync_miner(start_index, end_index, path){
     let str = "" + start_index;
     let ans = pad.substring(0, pad.length - str.length) + str;
     let file_name = "bootstrap-"+ans+".dat";
-
+    
+    console.log("download file name:", file_name, " ...");
     const file = fs.createWriteStream(path+"/"+file_name);
     const response = await axios.get("https://s3.amazonaws.com/dooglus/"+file_name);
     response.data.pipe(file);
+
+   console.log("done");
   }
 
 
