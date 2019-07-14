0b6f3456325735893ed02d6ddf0d660253fba37e
Ayesha Hafeez <ayesha.h@live.com>
2019-07-14 17:13:13 -0400

    removed comments
    Signed-off-by: Ayesha Hafeez <ayesha.h@live.com>

--- app/util/bitcoin_rpc_client.js
+++ app/util/bitcoin_rpc_client.js
@@ -3,7 +3,7 @@ var bitcoin_rpc = require('node-bitcoin-rpc')
 const { host, port, rpc_user, rpc_pass } = require('../../config').bitcoin_payment_config
 
 bitcoin_rpc.init(host, port,rpc_user, rpc_pass);
-// bitcoin_rpc.init('165.227.33.142', '18332', 'rpcbitcoin', 'u1O9o5yAhXgkQHix2kJFZJlVxF9BnbYmnYfcBtCRpRA=');
+
 const rpc_call = util.promisify(bitcoin_rpc.call);
 
 module.exports = {
--- app/util/clam_miner_rpc_client.js
+++ app/util/clam_miner_rpc_client.js
@@ -6,9 +6,6 @@ const { host, port, rpc_user, rpc_pass } = require('../../config').clamcoin_paym
 clam_coin_rpc = new Client({host:host,port:port, protocol:"http"})
 clam_coin_rpc.setBasicAuth(rpc_user, rpc_pass);
 
-// clam_coin_rpc = new Client({host:"68.183.196.159",port:"30174", protocol:"http"})
-// clam_coin_rpc.setBasicAuth("clamrpc", "EEqbenpLJW257wnSuzvvJAybjr4yfgBfTNQaGZtiTxSN");
-
 
 const rpc_call = function (method,param) {
   return new Promise(resolve => {
