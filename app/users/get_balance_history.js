de973717c58f5a50a5929773e17340c5989f757f
Ayesha Hafeez <ayesha.h@live.com>
2019-07-21 00:33:58 -0400

    Implemented API: Get User Balance History : get user's balance history in all owned investments
    Signed-off-by: Ayesha Hafeez <ayesha.h@live.com>

--- app/users/get_balance_history.js
+++ app/users/get_balance_history.js
@@ -1,23 +1,22 @@
-const dateFormat = require('dateformat');
-const { get_account_transactions_by_date } = require('../models').transaction_model
-const { get_account_by_id, account_balance } = require('../models').account_model
+const { get_accounts_per_user } = require('../models').account_model
 const { get_investment_by_id } = require('../models').investment_model
-const { get_quoted_rate } = require('../foreign_exchange/get_rate')
-const { getDates } = require('../util/common')
+const { get_balance_history } = require('../accounts/get_balance_history')
+
 /**
- * API to fetch the balance history for a specific account over a period of time
- * @param  {int} account_id     id of the account
- * @param  {string} time_period time period
+ * API to fetch the balance history for a specific user for all accounts over a period of time
+ * @param  {string} username     username of the user whose balance you want to fetch
+ * @param  {integer} time_period time period in days
  * @return {JSON}         Balance and Status
  */
 module.exports = async function balance_history_api(req, res) {
 
-  let account_id = req.body.account_id;
+
+  let username = req.body.username;
   let time_period_days = req.body.time_period_days;
 
   try{
     console.log("inside balance history");
-    let balance_history = await get_balance_history(account_id, time_period_days);
+    let balance_history = await get_user_balance_history(username, time_period_days);
     res.send({ code: "Success", balance_history })
   }
   catch(err){
@@ -28,122 +27,31 @@ module.exports = async function balance_history_api(req, res) {
 
 };
 
-async function get_balance_history(account_id, time_period_days){
+async function get_user_balance_history(username, time_period_days){
 
 
-  console.log("inside get_balance_history");
-    //parse time period
-    //find start date and end date
+  let user_accounts = await get_accounts_per_user(username);
+  console.log("user_accounts: ", user_accounts.length);
+  let user_balance_history = [];
 
-    let dateOffset = (24*60*60*1000) * time_period_days;
+  for(let i=0; i < user_accounts.length; i++){
 
-    // let end_date = new Date(new Date().setHours(0,0,0,0));
-    // let start_date = new Date(new Date().setHours(0,0,0,0));
-    let end_date = new Date();
-    let start_date = new Date();
-    start_date.setTime(start_date.getTime() - dateOffset);
+    let user_account = user_accounts[i];
+    let account_id = user_account.account_id;
+    let investment = await get_investment_by_id(user_account.investment_id)
 
-    //convert the date to mysql format
-    // start_date = start_date.toMysqlFormat();
-    // end_date = end_date.toMysqlFormat();
-    start_date = dateFormat(start_date,'yyyy-mm-dd');
-    end_date = dateFormat(end_date,'yyyy-mm-dd');
+    let account_balance_history = await get_balance_history(account_id, time_period_days);
+    user_balance_history.push(
+    {
+      account_id:user_account.account_id,
+      investment_id:user_account.investment_id,
+      investment_name:investment.investment_name,
+      account_history:account_balance_history
+    });
 
-    console.log("start_date ",start_date);
-    console.log("end_date ",end_date);
+  }
 
-   let account = await get_account_by_id(account_id);
-
-   //TODO: optimize it later to perform minimal db queries
-   let investment = await get_investment_by_id(account.investment_id);
-   let currency = investment.currency;
-
-
-
-   if(!account) throw new Error('Account does not exist');
-
-   let account_type = account.account_type;
-
-   //get the balance history between start and end date
-   let transactions = await get_account_transactions_by_date(account_id, start_date, end_date);
-
-   let transaction_history = [];
-   let balance = await account_balance(account_id);
-
-   for(let i=0; i<transactions.length; i++){
-     let account_transaction = transactions[i];
-
-     if(i!=0){
-       if (account_type == 'debit'){
-         //debits mean increase in balance
-         //credits mean decrease in balance
-         balance -= parseFloat(transactions[i-1].amount);
-       }
-       else {
-         //debits mean decrease in balance
-         //credits mean increase in balance
-         balance -= parseFloat(transactions[i-1].amount)*-1.0;
-       }
-     }
-
-     balance = parseFloat(balance.toFixed(8));
-
-
-
-
-     let transaction_json = {
-       'date':dateFormat(new Date(account_transaction.time),'dd mm yyyy'),
-       'account_balance':balance,
-       // 'account_balance_cad':balance_cad,
-       'currency':currency
-
-     };
-
-
-     transaction_history.push(transaction_json);
-
-
-   }//end for
-
-   //We have the transaction history at this point
-   console.log("transaction_history\n",transaction_history);
-
-   //get the latest exchange rate from the db src:investment currency, target: CAD
-   let quoted_rate = await get_quoted_rate(currency, 'CAD');
-   let exchange_rate = parseFloat(quoted_rate.bid);
-
-
-   let balance_history = [];
-   let dates = getDates(start_date,end_date).reverse();
-   let last_balance = transaction_history[0].account_balance;
-
-   for(let i=0; i<dates.length; i++){
-
-     //entries of current date
-     let relevant_entries = transaction_history.filter(function (el) {
-       // console.log(el.date,dates[i]);
-       return el.date == dates[i];
-     });
-
-     if(relevant_entries.length > 0){
-       last_balance = relevant_entries[0].account_balance
-     }
-
-     let balance_cad = parseFloat((exchange_rate * last_balance).toFixed(8));
-     balance_history.push({
-       date:dates[i],
-       account_balance: last_balance,
-       account_balance_cad: balance_cad
-
-     });
-
-     //remove the found entries, assuming transaction is sorted in descending order
-     transaction_history = transaction_history.splice(relevant_entries.length)
-
-   }
-
-
-   return balance_history;
+  return user_balance_history;
 
 
 }
