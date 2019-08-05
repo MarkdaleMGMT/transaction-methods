65ad711d373f76b3cf97b7f444ed06206f507316
Ayesha Hafeez <ayesha.h@live.com>
2019-08-04 20:24:55 -0400

    Added file for global config setup
    Signed-off-by: Ayesha Hafeez <ayesha.h@live.com>

--- startup_script.sql
+++ startup_script.sql
@@ -6,18 +6,3 @@ VALUES
 ('rake_user', '$2b$10$IuWpARdQuMBJHu2cnDIfdede0bZy428ViuvbK5MME4v4OOAWocNCW', 0, NULL, '', 1, NULL),
 ('fx_user', '$2b$10$IuWpARdQuMBJHu2cnDIfdede0bZy428ViuvbK5MME4v4OOAWocNCW', 0, NULL, '', 1, NULL),
 ('withdrawal_user','$2b$10$IuWpARdQuMBJHu2cnDIfdede0bZy428ViuvbK5MME4v4OOAWocNCW', 0, NULL, '', 1, NULL);
-
-
-
-INSERT INTO global_config
-(param, value)
-VALUES
-('bitcoin_change_address', ''),
-('bitcoin_cold_storage', ''),
-('bitcoin_tx_fee', ''),
-('bitcoin_withdrawal_fee_percent', ''),
-('clamcoin_change_address', ''),
-('clamcoin_cold_storage', ''),
-('clamcoin_tx_fee', ''),
-('clamcoin_withdrawal_fee_percent', ''),
-('quote_pct', '');
