59deb8380b9bc120791857931d8726259d9dff7b
Ayesha Hafeez <ayesha.h@live.com>
2019-09-05 23:52:02 -0400

    global update crontask implemented
    Signed-off-by: Ayesha Hafeez <ayesha.h@live.com>

--- app/crontasks/index.js
+++ app/crontasks/index.js
@@ -1,6 +1,7 @@
 const { backup_database } = require('./backup_db');
 const scrape_fx_rate_crontasks = require('./scrape_fx_rates')
 const calculate_rate_crontasks = require('./calculate_fx_rate')
+const update_investment_balance = require('../transactions/global_update_all_investments')
 const cron = require("node-cron");
 
 // backup_database();
@@ -11,22 +12,22 @@ async function schedule_and_run_crontasks(){
   //mail a db backup every midnight
   // cron.schedule("0/1 0 * * *",backup_db);
 
+  //run global update for all investments
+  cron.schedule("0 */1 * * * *", update_investment_balance);
 
 
-  /*let scrape_fx_crons = await scrape_fx_rate_crontasks();
-  // console.log("orderbook_crons ",orderbook_crons);
-  for(let i=0; i<scrape_fx_crons.length; i++){
+  //scrape raw rates
+  // let scrape_fx_crons = await scrape_fx_rate_crontasks();
+  // for(let i=0; i<scrape_fx_crons.length; i++){
+  //   cron.schedule(scrape_fx_crons[i].cron_interval,()=>{ scrape_fx_crons[i].cron_fn(); });
+  // }
 
-  cron.schedule(scrape_fx_crons[i].cron_interval,()=>{ scrape_fx_crons[i].cron_fn(); });
-  //cron.schedule(scrape_fx_crons[i].cron_interval,()=>{ scrape_fx_crons("Running Task ",i); });
-}*/
 
-  let calculate_rate_crons = await calculate_rate_crontasks();
-  for(let i=0; i<calculate_rate_crons.length; i++){
-
-  cron.schedule(calculate_rate_crons[i].cron_interval,()=>{ calculate_rate_crons[i].cron_fn(); });
-  //cron.schedule(scrape_fx_crons[i].cron_interval,()=>{ scrape_fx_crons("Running Task ",i); });
-  }
+  //calculate rates
+  // let calculate_rate_crons = await calculate_rate_crontasks();
+  // for(let i=0; i<calculate_rate_crons.length; i++){
+  //   cron.schedule(calculate_rate_crons[i].cron_interval,()=>{ calculate_rate_crons[i].cron_fn(); });
+  // }
 
 
 
--- app/transactions/global_update_all_investments.js
+++ app/transactions/global_update_all_investments.js
@@ -4,12 +4,16 @@ const { update_investment_balance } =require('./global_update');
 
 const bcrypt = require('bcrypt');
 
-/**
- * API for running global update for all investments with API access info
- * @param  {str} username     Initiator of the global update
- * @param  {str} password     password of user 'username'
- * @return {JSON}         Returns status of global update of individual assesment
- */
+
+
+// /**
+//  * API for running global update for all investments with API access info
+//  * @param  {str} username     Initiator of the global update
+//  * @param  {str} password     password of user 'username'
+//  * @return {JSON}         Returns status of global update of individual assesment
+//  */
+
+ /*
 module.exports = async function global_update_multi_investments(req, res){
 
   try{
@@ -23,27 +27,27 @@ module.exports = async function global_update_multi_investments(req, res){
     console.error(err);
     res.status(400).send({msg: 'Error in updating investment balance', error:err.message});
   }
-};
+};*/
 
-async function update_multi_investments_balance(username, password){
+module.exports = async function update_investments_balance(){
 
     let result = [];
 
     //check if user is admin and if the password hash matches that in the db
-    let user = await get_user_by_username(username);
+    //let user = await get_user_by_username(username);
 
     //if not authorized or authenticated, throw and error
-    if(!user || user.level!=0){
-     throw new Error('Not authorized to initiate global update');
-    }
+    // if(!user || user.level!=0){
+    //  throw new Error('Not authorized to initiate global update');
+    // }
 
-    console.log(password, user.password);
-    const match = await bcrypt.compare(password, user.password);
-
-    console.log("match", match);
-    if(!match){
-      throw new Error('Authentication failed');
-    }
+    // console.log(password, user.password);
+    // const match = await bcrypt.compare(password, user.password);
+    //
+    // console.log("match", match);
+    // if(!match){
+    //   throw new Error('Authentication failed');
+    // }
 
     //iterate over all the investments that have API access info
     let datetime = new Date().toMysqlFormat()
@@ -58,6 +62,7 @@ async function update_multi_investments_balance(username, password){
       console.log("running global update for investment_id ", investment_id, " ....");
       //get the balance
       let balance = await get_balance(investment_id);
+      if(!balance) continue;
 
       //run global update
       let isSuccesful = await update_investment_balance(username,investment_id,balance,datetime);
