4c8e9d2f2a68a494f04f7d0a5bc382ae2993918c
Ayesha Hafeez <ayesha.h@live.com>
2019-09-12 13:09:54 -0400

    changed account details API to return bid and ask price:

--- app/users/get_account.js
+++ app/users/get_account.js
@@ -32,11 +32,24 @@ async function get_account_details(username, investment_id){
   let investment = await get_investment_by_id(investment_id);
 
   let quoted_rate = await get_quoted_rate(investment.currency, base_currency);
-  let exchange_rate = quoted_rate.from_to == investment.currency+'_'+base_currency ? parseFloat(quoted_rate.bid) : parseFloat(1/quoted_rate.ask);
+  let exchange_rate = {};
+
+  if(quoted_rate.from_to == investment.currency+'_'+base_currency){
+    exchange_rate = quoted_rate
+  }
+  else{
+    exchange_rate = {
+      bid: parseFloat(1/quoted_rate.ask),
+      ask: parseFloat(1/quoted_rate.bid),
+      mid: parseFloat(1/quoted_rate.mid)
+    }
+  }
+  delete exchange_rate.from_to;
+  // let exchange_rate = quoted_rate.from_to == investment.currency+'_'+base_currency ? parseFloat(quoted_rate.bid) : parseFloat(1/quoted_rate.ask);
 
   let account_balance_val = await account_balance(account.account_id);
   //round CAD to 2dp
-  let account_balance_cad = parseFloat(account_balance_val * exchange_rate).toFixed(2);
+  let account_balance_cad = parseFloat(account_balance_val * exchange_rate.bid).toFixed(2);
 
   return {...account, account_balance: account_balance_val, account_balance_cad, exchange_rate};
 
