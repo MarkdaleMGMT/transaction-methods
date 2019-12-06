7545bb9b0451398002e2486e54be2e9dff3f083b
Adam Zartin <a.zartin@mail.utoronto.ca>
2019-12-06 18:00:16 -0500

    temp fix for currency rates

---
+++ app/foreign_exchange/new_get_rates_history.js
@@ -0,0 +1,35 @@
+const moment = require('moment');
+
+const { get_all_currencies } = require('../models').investment_model;
+const { base_currency } = require('../../config');
+const { get_currency_rates_history} = require('../models').fx_quoted_rates;
+
+module.exports = async function get_rates_history_api(req, res){
+
+  try{
+    let time_period_days = req.body.time_period_days;
+    let rates_history = await get_rates_history(time_period_days);
+    res.send({ code: "success", rates_history })
+  }
+  catch(err){
+    console.error("got err",err);
+    res.status(400).send({code:"failure" , message:err.message});
+  }
+
+
+}
+
+async function get_rates_history(time_period_days){
+  let currencies = await get_all_currencies();
+  
+  let rates_history = [];
+
+  for(let i=0; i<currencies.length; i++){
+    let rates = await get_currency_rates_history(currencies[i], base_currency, time_period_days)
+    rates_history.push({ currency: currencies[i], rates: rates });
+
+  } 
+
+  return rates_history;
+
+}
