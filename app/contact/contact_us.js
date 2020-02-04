21e1f7a96126b0d6ee42fcb6592c47d9cf14c7dd
Adam Zartin <a.zartin@mail.utoronto.ca>
2020-02-04 15:28:24 -0500

    contact us api v1

--- app/contact/contact_us.js
+++ app/contact/contact_us.js
@@ -8,20 +8,21 @@ module.exports = async function create_account(req, res) {
     let user_body = req.body.body
 
     try{
+        //send an email to db_backup about the contact form
         let text = create_text(user_email, user_subject, user_body)
         let to_admin = await send_email(db_backup.email, `[Contact] [${user_email}] ${user_subject}`, text, null);
-        console.log("email result: ", to_admin);
-        res.send("Success")
+        console.log("email_admin result: ", to_admin);
+        
+        //send an email to the user that their email has been recieved
+        text = `This is an email to confirm that we have received your message. A member from our team will contact you soon regarding your concern!\n`
+        let to_user = await send_email(user_email, `[Do Not Reply] We have recieved your message!`, text, null);
+        console.log("email_user result: ", to_user);
+
+        res.send({ code: "Our team has been notified! We'll get back to you soon"})
     } catch (err) {
-        res.send(err)
+        res.status(400).send({code:"Something went wrong with sending the mail" , message:err.message})
     }
-    //send an email to the user that their email has been recieved
     
-
-    //send an email to db_backup about the contact form
-
-  
- 
   };
 
 function create_text(email, subject, body){
