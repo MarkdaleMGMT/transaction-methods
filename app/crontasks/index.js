a6702ecf11bcbc2621b133681ebd1d541f7ae57e
zartinad <zartinight@gmail.com>
2020-03-26 01:20:33 +0000

    force global update

--- app/crontasks/index.js
+++ app/crontasks/index.js
@@ -10,11 +10,12 @@ const cron = require("node-cron");
 async function schedule_and_run_crontasks(){
 
   //mail a db backup every midnight
-  cron.schedule("0 0 * * *",backup_database);
+  //cron.schedule("0 0 * * *",backup_database);
 
   //run global update for all investments
-  cron.schedule("0 0 * * *", update_investment_balance);
+  //cron.schedule("0 1 * * *", update_investment_balance);
 
+  cron.schedule("0 0 * * *", midnight_jobs)
 
   //scrape raw rates
   let scrape_fx_crons = await scrape_fx_rate_crontasks();
@@ -31,6 +32,17 @@ async function schedule_and_run_crontasks(){
 
 
 
+}
+
+midnight_jobs = async () => {
+	console.log("[Cron Task] Running Midnight Tasks")
+	console.log("[Cron Task] Sending Backup Database")
+	await backup_database();
+	console.log("[Cron Task] Completed Backup Database")
+
+	console.log("[Cron Task] Starting Global Update")
+	await update_investment_balance();
+	console.log("[Cron Task] Completed Global Update")
 }
 
 
