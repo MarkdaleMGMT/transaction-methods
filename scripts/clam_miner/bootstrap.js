cbf9097205afcd53ee5734b622db7e024906bc44
ayeshah <ayesha.h@live.com>
2020-05-03 17:11:44 -0400

    Update bootstrap.js
    to automatically download bootstrap files for clam miner

--- scripts/clam_miner/bootstrap.js
+++ scripts/clam_miner/bootstrap.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 const axios = require("axios");
 
@@ -8,13 +9,13 @@ async function sync_miner(start_index, end_index, path){
 
   for(let i=start_index; i<= end_index; i++){
 
-    let str = "" + start_index;
+    let str = "" + i;
     let ans = pad.substring(0, pad.length - str.length) + str;
     let file_name = "bootstrap-"+ans+".dat";
-    
+
     console.log("download file name:", file_name, " ...");
     const file = fs.createWriteStream(path+"/"+file_name);
-    const response = await axios("https://s3.amazonaws.com/dooglus/"+file_name, method: "GET", responseType: "stream");
+    const response = await axios({url:"https://s3.amazonaws.com/dooglus/"+file_name, method:"GET", responseType:"stream"});
     response.data.pipe(file);
 
    console.log("done");
@@ -23,4 +24,4 @@ async function sync_miner(start_index, end_index, path){
 
 }
 
-sync_miner(184, 257,'/root/bootstrap-files')
+sync_miner(275, 305,'./bootstrap_files')
